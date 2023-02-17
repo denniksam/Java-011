ad8123b69524e2017caac41e853a503a2a06508c
Denys Samoilenko <denniksam@gmail.com>
2023-02-17 19:22:10 +0200

    Forms -- 1

--- WebBasics/src/main/java/itstep/learning/servlet/FormsServlet.java
+++ WebBasics/src/main/java/itstep/learning/servlet/FormsServlet.java
@@ -7,16 +7,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 public class FormsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        FormsModel model = new FormsModel() ;
-        model.setMessage( "Message from FormsServlet" ) ;
-        req.setAttribute( "formsModel", model ) ;
+        req.setCharacterEncoding( "UTF-8" ) ;  // установка кодировки чтения из запроса. ДО ПЕРВОГО ЧТЕНИЯ
+        resp.setCharacterEncoding( "UTF-8" ) ;
 
+        FormsModel model = parseModel( req ) ;
+        req.setAttribute( "formsModel", model ) ;
         req.getRequestDispatcher( "WEB-INF/forms.jsp" )
                 .forward( req, resp ) ;
+    }
+
+    private FormsModel parseModel( HttpServletRequest req ) {
+        FormsModel model = new FormsModel() ;
+        model.setMethod( req.getMethod() ) ;
+        // region "text"
+        String text = req.getParameter( "text" ) ;   // "text" - name of <input name="text" .../>
+        if( text == null ) {   // нет параметра (скорее всего нет передачи формы)
+            model.setText( "NULL" ) ;
+        }
+        else if( text.isEmpty() ) {   // параметр передан, но значение отсутствует
+            model.setText( "empty" ) ;
+        }
+        else {
+            model.setText( text ) ;
+        }
+        // endregion
+        // region "number"
+        String numberParam = req.getParameter( "number" ) ;
+        if( numberParam == null ) {
+            model.setNumber( -1 ) ;
+        }
+        else if( numberParam.isEmpty() ) {
+            model.setNumber( 0 ) ;
+        }
+        else {
+            try { model.setNumber( Double.parseDouble( numberParam ) ) ; }
+            catch( NumberFormatException ignored ) { model.setNumber( -0.1 ) ; }
+        }
+        // endregion
+        // region "date"
+        String dateParam = req.getParameter( "date" ) ;
+        if( dateParam == null ) {
+            model.setDate( null ) ;
+        }
+        else if( dateParam.isEmpty() ) {
+            model.setDate( new Date() ) ;
+        }
+        else {
+            String dateFormat = "yyyy-MM-dd" ;
+            SimpleDateFormat format = new SimpleDateFormat( dateFormat ) ;
+            try { model.setDate( format.parse( dateParam ) ) ; }
+            catch( ParseException ignored ) { model.setDate( Date.from( Instant.EPOCH ) ) ; }
+        }
+        // endregion
+        return model ;
     }
 }
 /*
--- WebBasics/target/WebBasics/WEB-INF/forms.jsp
+++ WebBasics/target/WebBasics/WEB-INF/forms.jsp
@@ -2,14 +2,62 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <%
     FormsModel model = (FormsModel) request.getAttribute( "formsModel" ) ;
+    String path = request.getContextPath() ;
 %>
 <html>
 <head>
     <title>Forms</title>
 </head>
+<style>
+    .container{
+        width: 80%;
+        border: 1px solid green;
+    }
+    .d-flex{
+        display: flex;
+    }
+    .f-col{
+        display: flex;
+        flex-direction: column;
+    }
+    .mb-2{
+        margin-bottom: 2em;
+    }
+    .m-auto{
+        margin: auto;
+    }
+    .maxW-200{
+        max-width: 200px;
+    }
+    .minH-200{
+        min-height: 200px;
+    }
+</style>
 <body>
-<h1>Передача данных. Формы</h1>
-
-<%= model.getMessage() %>
+<div class="d-flex">
+    <div>
+        <form action="<%= path %>/forms" method="post" class="f-col mb-2 maxW-200">
+            <label>Text:   <input name="text"   type="text"></label>
+            <label>Number: <input name="number" type="number" step="0.01"></label>
+            <input name="date"   type="date">
+            <input name="color"  type="color">
+            <input type="submit" value="Post Form">
+        </form>
+        <form action="<%= path %>/forms" method="get" class="f-col mb-2 maxW-200">
+            <label>Text:   <input name="text"   type="text"></label>
+            <label>Number: <input name="number" type="number" step="0.01"></label>
+            <input name="date"   type="date">
+            <input name="color"  type="color">
+            <input type="submit" value="Get Form">
+        </form>
+    </div>
+    <div class="container m-auto minH-200" >
+        Method = <%= model.getMethod() %> <br>
+        Text   = <%= model.getText() %>   <br>
+        Number = <%= model.getNumber() %> <br>
+        Date   = <%= model.getDate() %>   <br>
+        Color  = <%= model.getColor() %>  <br>
+    </div>
+</div>
 </body>
 </html>
