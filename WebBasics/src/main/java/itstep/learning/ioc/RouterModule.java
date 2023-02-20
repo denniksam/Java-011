ac65986d4708e06ae0df207f5f66e7ee12b824ed
Denys Samoilenko <denniksam@gmail.com>
2023-02-20 21:00:17 +0200

    Web IoC -- 2

--- WebBasics/src/main/java/itstep/learning/ioc/RouterModule.java
+++ WebBasics/src/main/java/itstep/learning/ioc/RouterModule.java
@@ -2,6 +2,7 @@ package itstep.learning.ioc;
 
 import com.google.inject.servlet.ServletModule;
 import itstep.learning.filter.CharsetFilter;
+import itstep.learning.filter.DbCheckFilter;
 import itstep.learning.servlet.AboutServlet;
 import itstep.learning.servlet.FormsServlet;
 import itstep.learning.servlet.HomeServlet;
@@ -11,6 +12,7 @@ public class RouterModule extends ServletModule {
     protected void configureServlets() {
         // задаем фильтры
         filter( "/*" ).through( CharsetFilter.class ) ;
+        filter( "/*" ).through( DbCheckFilter.class ) ;
 
         // и сервлеты
         serve( "/home"  ).with( HomeServlet.class  ) ;
--- WebBasics/src/main/java/itstep/learning/ioc/ServiceModule.java
+++ WebBasics/src/main/java/itstep/learning/ioc/ServiceModule.java
@@ -1,10 +1,12 @@
 package itstep.learning.ioc;
 
 import com.google.inject.AbstractModule;
+import itstep.learning.service.DbService;
+import itstep.learning.service.LocalDbService;
 
 public class ServiceModule extends AbstractModule {
     @Override
     protected void configure() {
-
+        bind( DbService.class ).to( LocalDbService.class ) ;
     }
 }
